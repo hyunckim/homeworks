6e1312e4cd9d47a7acaa5d12a1b205822f6c74f0
hyunckim <hyunkim1209@gmail.com>
2017-01-27 00:12:57 -0800

    Finish Map class

--- W1D4/ADT_hw.rb
+++ W1D4/ADT_hw.rb
@@ -37,3 +37,40 @@ class Queue
     @queue.dup
   end
 end
+
+#Exercise 3
+class Map
+  def initialize
+    @my_map = []
+  end
+
+  def assign(key, value)
+    if lookup(key).nil?
+      @my_map << [key, value]
+    else
+      @my_map.each do |pair|
+        pair[1] = value if pair[0] == key
+      end
+    end
+  end
+
+  def lookup(key)
+    @my_map.each do |pair|
+      return pair[1] if pair[0] == key
+    end
+    nil
+  end
+
+  def remove(key)
+    value = lookup(key)
+
+    return value if value.nil?
+
+    @my_map - [[key, value]]
+    [key, value]
+  end
+
+  def show
+    @my_map.dup
+  end
+end
