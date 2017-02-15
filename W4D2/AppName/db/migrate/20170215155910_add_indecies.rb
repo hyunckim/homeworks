15220efdfb5da9e85b9cec6243f2ed927f888eea
hyunckim <hyunkim1209@gmail.com>
2017-02-15 08:13:59 -0800

    Add rails auth homework

--- W4D2/AppName/db/migrate/20170215155910_add_indecies.rb
+++ W4D2/AppName/db/migrate/20170215155910_add_indecies.rb
@@ -2,6 +2,5 @@ class AddIndecies < ActiveRecord::Migration
   def change
     add_index :users, :username, unique: true
     add_index :users, :session_token, unique: true
-    add_column :users, :password, :string
   end
 end
