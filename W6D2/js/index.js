e4dd09a6d672d3c5fe586c5dd2063175bed43515
hyunckim <hyunkim1209@gmail.com>
2017-02-28 22:28:57 -0800

    Add first AJAX request

--- W6D2/js/index.js
+++ W6D2/js/index.js
@@ -1,5 +1,15 @@
 console.log("Hello from the JavaScript console!");
 
+$.ajax({
+  type: 'GET',
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  success(data) {
+    console.log(data);
+  }
+});
+
+console.log("hi");
+
 // Your AJAX request here
 
 // Add another console log here, outside your AJAX request
