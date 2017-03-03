957d588d44ac20614d55ce140e4bb1fe7c516265
hyunckim <hyunkim1209@gmail.com>
2017-03-03 01:11:09 -0800

    initial commit

--- W6D4/calculator_skeleton/calculator.jsx
+++ W6D4/calculator_skeleton/calculator.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-class Calculator extends React.Component {
-  constructor(props) {
+class Calculator extends React.Component{
+  constructor(props){
     super(props);
+    //your code here
   }
 
-  render() {
+  //your code here
+
+  render(){
     return (
       <div>
         <h1>Hello World</h1>
+        //your code will replace this
       </div>
     );
   }
