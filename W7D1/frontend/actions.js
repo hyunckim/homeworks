165d1d997e4dd4f37cce0bf11ee43b0ccb088783
hyunckim <hyunkim1209@gmail.com>
2017-03-04 22:24:32 -0800

    Add reduce

--- W7D1/frontend/actions.js
+++ W7D1/frontend/actions.js
@@ -1,6 +1,6 @@
 const SWITCH_CURRENCY = "SWITCH_CURRENCY";
 
-const selectCurrency = (baseCurrency, rates) => ({
+export const selectCurrency = (baseCurrency, rates) => ({
   type: SWITCH_CURRENCY,
   baseCurrency: baseCurrency,
   rates: rates
