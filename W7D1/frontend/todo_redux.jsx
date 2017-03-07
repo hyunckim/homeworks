1267f49f2f38b71a6a81d8ed65f4fd567d2182af
hyunckim <hyunkim1209@gmail.com>
2017-03-06 21:09:27 -0800

    add #addLoggingToDispatch middleware

--- W7D1/frontend/todo_redux.jsx
+++ W7D1/frontend/todo_redux.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import { applyMiddleware } from 'redux';
 
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
+  const store = configureStore(preloadedState, applyMiddleware);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+const addLoggingToDispatch = store => next => action => {
+  const dispatch = store.dispatch;
+  console.log("state pre-dispatched: ", store.getState());
+  console.log("action:", action);
+  let returnValue = dispatch(action);
+  console.log("state after-dispatched: ",store.getState());
+  return returnValue;
+};
