e1de71ce78765f80463cdfba50bed82a5ac986ba
hyunckim <hyunkim1209@gmail.com>
2017-03-07 23:23:57 -0800

    Add Root Compoenent and set-up Giphys Search Container

--- W7D2/giphy_search.jsx
+++ W7D2/giphy_search.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { fetchSearchGiphys } from './actions/giphy_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
 
   const store = configureStore();
-  window.store = store;
-  window.fetchSearchGiphys = fetchSearchGiphys;
+
+  ReactDOM.render(<Root store={store} />, document.getElementById('root'));
 
 });
